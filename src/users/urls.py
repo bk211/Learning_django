693998aae6fde4725a6ef4bf3ba753b61cf2e7e0
playmaker <chaoleicai@gmail.com>
2025-05-03 19:03:21 +0200

    setup login page

--- src/users/urls.py
+++ src/users/urls.py
@@ -20,15 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from main import urls as main_app_urls
-from users import urls as users_app_urls
+from .views import login_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(main_app_urls)),
-    path('', include(users_app_urls)),
-
+    path('login/', login_view, name="login" ),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
